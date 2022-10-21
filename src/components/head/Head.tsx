ffa05ede531c11931c9e5ffc0bda5654c05e3c17
kong <duguyihou@gmail.com>
2022-10-22 09:07:52 +1100

    feat: add head and footer

---
+++ src/components/head/Head.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+const NextHead = () => {
+  return (
+    <Head>
+      <title>Rhythm</title>
+      <meta name="description" content="radio" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  )
+}
+
+export default NextHead
