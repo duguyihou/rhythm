e2b984c4a46e51ccdf1ed68c3d9f5d868f85ac12
kong <duguyihou@gmail.com>
2022-10-23 12:18:30 +1100

    feat: sidebar

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
+import { Sidebar } from '../sidebar'
 import { MainLayoutProps } from './MainLayout.types'
 
 const MainLayout = ({ children }: MainLayoutProps) => {
   return (
     <div className="h-screen flex overflow-hidden bg-gray-100">
+      <Sidebar />
       <main className="flex-1 relative overflow-y-auto focus:outline-none">
         {children}
       </main>
