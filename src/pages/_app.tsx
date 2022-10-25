76ae36ecc6da44caea1289a48d68f80bca9c14c9
kong <duguyihou@gmail.com>
2022-10-25 16:07:24 +1100

    chore: test github actions

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,21 @@
 import '../index.css'
 
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import type { AppProps } from 'next/app'
 
 import { MainLayout } from '../components/layout'
-import AppProvider from '../providers/AppProvider'
+
+const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <AppProvider>
+    <QueryClientProvider client={queryClient}>
       <MainLayout>
         <Component {...pageProps} />
       </MainLayout>
-    </AppProvider>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   )
 }
 
