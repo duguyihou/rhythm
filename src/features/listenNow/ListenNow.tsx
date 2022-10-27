93d0c5ad36886c19907f147b0e66bcd64f8e905c
kong <duguyihou@gmail.com>
2022-10-28 07:14:59 +1100

    feat: radio card

--- src/features/listenNow/ListenNow.tsx
+++ src/features/listenNow/ListenNow.tsx
@@ -1,5 +1,13 @@
+import { RadioList } from '../radio/components'
+import { Filter } from '../radio/components/radioList/RadioList.types'
+
 const ListenNow = () => {
-  return <h1 className="text-lg underline">Player</h1>
+  return (
+    <>
+      <RadioList filter={Filter.topvote} />
+      <RadioList filter={Filter.topclick} />
+    </>
+  )
 }
 
 export default ListenNow
