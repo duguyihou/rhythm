24011c5ad0ab232ae25fe29d0f5fef6579f052fc
kong <duguyihou@gmail.com>
2022-11-06 06:22:09 +1100

    feat: wip

---
+++ src/features/stations/hooks/useAllStationList.ts
@@ -0,0 +1,19 @@
+import { useQuery } from '@tanstack/react-query'
+
+import configService from '../../../config'
+import { Station } from '../components'
+import { stationKeys } from './stationKeys'
+
+export const useAllStationList = () => {
+  const queryResult = useQuery<Station[], Error>({
+    queryKey: stationKeys.all,
+    queryFn: async () => {
+      const url = `${configService.baseUrl}/stations`
+      const res = await fetch(url)
+      const result = await res.json()
+      return result
+    },
+    suspense: true,
+  })
+  return queryResult
+}
