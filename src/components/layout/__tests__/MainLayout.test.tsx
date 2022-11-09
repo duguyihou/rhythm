ef9493e5ef1d88b985c2fa3794e3016ada6e93de
kong <duguyihou@gmail.com>
2022-11-09 15:24:16 +1100

    test: mainLayout & searchStation

--- src/components/layout/__tests__/MainLayout.test.tsx
+++ src/components/layout/__tests__/MainLayout.test.tsx
@@ -1,10 +1,21 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { render } from '@testing-library/react'
 
 import MainLayout from '../MainLayout'
-
+jest.mock('next/router', () => ({
+  useRouter: () => ({
+    pathname: '/',
+  }),
+}))
 test('mainLayout', () => {
+  const queryClient = new QueryClient()
   const children = <h1>mainLayout</h1>
-  const mainLayout = render(<MainLayout>{children}</MainLayout>)
+
+  const mainLayout = render(
+    <QueryClientProvider client={queryClient}>
+      <MainLayout>{children}</MainLayout>
+    </QueryClientProvider>
+  )
 
   expect(mainLayout).toMatchSnapshot()
 })
---
+++ src/features/stations/components/searchStation/__tests__/SearchStation.test.tsx
@@ -0,0 +1,15 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { render } from '@testing-library/react'
+
+import SearchStation from '../SearchStation'
+
+test('searchStation', () => {
+  const queryClient = new QueryClient()
+  const searchStation = render(
+    <QueryClientProvider client={queryClient}>
+      <SearchStation />
+    </QueryClientProvider>
+  )
+
+  expect(searchStation).toMatchSnapshot()
+})
