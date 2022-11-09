8a28b080ffdd8a129f1639d8e888fcfcf752eae5
kong <duguyihou@gmail.com>
2022-11-09 16:57:23 +1100

    style: hide scroll bar

--- src/components/layout/ContentLayout.tsx
+++ src/components/layout/ContentLayout.tsx
@@ -21,7 +21,7 @@ const ContentLayout = (contentLayoutProps: ContentLayoutProps) => {
         <div className="p-2">{title}</div>
         {currentPlaying && <Player station={currentPlaying} />}
       </div>
-      <>{children}</>
+      <div className="scrollbar-hide">{children}</div>
     </div>
   )
 }
