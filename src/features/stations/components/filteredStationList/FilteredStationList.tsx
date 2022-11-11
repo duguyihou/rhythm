5fc0e3d55c000fb49e2cb9bf4fe22bbaff38ebfa
kong <duguyihou@gmail.com>
2022-11-11 14:37:40 +1100

    style: station item

--- src/features/stations/components/filteredStationList/FilteredStationList.tsx
+++ src/features/stations/components/filteredStationList/FilteredStationList.tsx
@@ -10,7 +10,7 @@ import { Filter, FilteredStationListProps } from './FilteredStationList.types'
 const FilteredStationList = (
   filteredStationListProps: FilteredStationListProps
 ) => {
-  const { filter, offset = 8 } = filteredStationListProps
+  const { filter, offset = 9 } = filteredStationListProps
   // TODO: extract a util function with generic type
   const filterVar = Object.keys(Filter)[
     Object.values(Filter).indexOf(filter as Filter)
@@ -37,7 +37,16 @@ const FilteredStationList = (
           <div className="cursor-pointer">See All</div>
         </Link>
       </section>
-      <div className={clsx('grid grid-flow-row gap-4 grid-cols-4')}>
+      <div
+        className={clsx(
+          'grid grid-flow-row gap-2',
+          '2xl:grid-cols-3',
+          'xl:grid-cols-2',
+          'lg:grid-cols-2',
+          'md:grid-cols-1',
+          'sm:grid-cols-1'
+        )}
+      >
         {data?.map((station) => (
           <StationItem key={station.stationuuid} {...station} />
         ))}
